01a53daa7047ff4e727b07fb45d202871ba4ab4b
Noah Teubner <nteubner@schueler.hakzell.at>
2023-10-21 00:20:08 +0200

    i do not understand what i am doing

--- Master.java
+++ Master.java
@@ -5,10 +5,21 @@ public class Master{
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("What is your name?");
+        System.out.println("What is your name? ");
         String name = scanner.nextLine();
 
+        System.out.println("How old are you? ");
+        int age = scanner.nextInt();
+
+        System.out.println("What is your favourite food? ");
+        String food = scanner.nextLine();
+
         System.out.println("Hello "+ name);
+        System.out.println("You are "+age+" years old");
+        System.out.println("You like "+food);
+
+       
+        
 
 
 
